786ba955802f486c71aeb9955cf606f5373efb77
harjot94179 <52996114+harjot94179@users.noreply.github.com>
2021-11-22 17:35:22 +0530

    Update settings.py

--- lets_quiz/settings.py
+++ lets_quiz/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
@@ -129,6 +136,11 @@ STATIC_URL = '/static/'
 #     # will not be served, long term storage
 #     os.path.join(BASE_DIR, "static"),
 # ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
